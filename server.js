4e0ccfac8c28906d2b587b816c6274ae2cdcba4a
MatheusDiniz <madtheusss@hotmail.com>
2023-07-04 22:42:12 -0300

    Insert express in project

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-const http = require('http');
-const port = 3000;
+import app from "./src/app.js"
 
-const routes = {
-    '/': 'Initial Page',
-    '/livros': 'BookPage',
-    '/autores': 'AuthorList',
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end(routes[req.url]);
-});
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Ta funfando na porta http://localhost:${port}`)
 })
 
